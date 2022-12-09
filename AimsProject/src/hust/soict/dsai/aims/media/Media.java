a263ef6096738c92805e698e20b54510204600b9
ntkien36 <81414174+ntkien36@users.noreply.github.com>
2022-12-09 21:56:56 +0700

    1

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -0,0 +1,51 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.Comparator;
+
+public abstract class Media {
+	public Media() {
+		// TODO Auto-generated constructor stub
+	}
+    protected int id;
+    protected String title;
+    protected String category;
+    protected float cost;
+    public Media(String title) {
+        this();
+        this.title = title;
+    }
+
+    public Media(String title, String category, float cost) {
+        this(title);
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public abstract String toString();
+    
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST = 
+    		new MediaComparatorByTitleCost();
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE = 
+    		new MediaComparatorByCostTitle();
+    @Override
+    public boolean equals(Object o){
+        Media tmp = (Media)o;
+        return this.title.equals(tmp.getTitle());
+    }
+}
